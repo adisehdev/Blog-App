e2ed4757f112379df1b6fd92281b9330cc64c27b
adisehdev <sehdevaditya@gmail.com>
2024-09-01 12:10:50 +0530

    making changes to create cookie when logging in so that it can work on deplyed url as well

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,12 +4,13 @@ const bcrypt = require("bcrypt");
 const path = require("path");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
-require('dotenv').config()
-const privateKey = process.env.PRIVATE_KEY
+require("dotenv").config();
+const privateKey = process.env.PRIVATE_KEY;
 
-const publicKey = process.env.PUBLIC_KEY
+const publicKey = process.env.PUBLIC_KEY;
 
-exports.registerUSer = async (req, res) => { //api to register new user
+exports.registerUSer = async (req, res) => {
+  //api to register new user
   try {
     let { username, password } = req.body;
     password = bcrypt.hashSync(password, 10);
@@ -17,11 +18,14 @@ exports.registerUSer = async (req, res) => { //api to register new user
     const registeredUser = await newUser.save();
     res.status(201).json(registeredUser);
   } catch (error) {
-    res.status(400).json({ error: "could not register user : " + error.message });
+    res
+      .status(400)
+      .json({ error: "could not register user : " + error.message });
   }
 };
 
-exports.loginUser = async (req, res) => { //api to login user
+exports.loginUser = async (req, res) => {
+  //api to login user
   try {
     let { username, password } = req.body;
     const user = await User.findOne({ username });
@@ -33,9 +37,16 @@ exports.loginUser = async (req, res) => { //api to login user
       const secret = privateKey;
       const callback = (err, token) => {
         if (err) {
-          res.status(400).json({error : "could not login user : , " + err})
-          throw err;}
-        res.cookie("token", token).json(payload); //putting token inside cookie
+          res.status(400).json({ error: "could not login user : , " + err });
+          throw err;
+        }
+        res
+          .cookie("token", token, {
+            httpOnly: true,
+            secure: true, // set to true if using HTTPS
+            sameSite: "None",
+          })
+          .json(payload); //putting token inside cookie
       };
       jwt.sign(payload, secret, { algorithm: "RS256" }, callback);
     } else {
@@ -46,15 +57,17 @@ exports.loginUser = async (req, res) => { //api to login user
   }
 };
 
-exports.getProfile = async (req, res) => { //api to get profile of user
+exports.getProfile = async (req, res) => {
+  //api to get profile of user
   //cookie se profile nikalega
   try {
     const { token } = req.cookies; //token created
 
     jwt.verify(token, publicKey, { algorithms: "RS256" }, (err, info) => {
       //extract payload from token
-      if (err) {console.log("error in getting profile : ",err);
-        
+      if (err) {
+        console.log("error in getting profile : ", err);
+
         throw err;
       }
       res.status(200).json(info); //info me payload pada he
@@ -65,7 +78,8 @@ exports.getProfile = async (req, res) => { //api to get profile of user
   }
 };
 
-exports.logoutUser = async (req, res) => { //api to logout user
+exports.logoutUser = async (req, res) => {
+  //api to logout user
   try {
     res.cookie("token", "").json({ cookies: res.cookies }).status(200); //resetting cookie
   } catch (error) {
